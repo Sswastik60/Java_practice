f0dd272a30c919956f0eb212d94fcc83f4bfe93e
Sswastik60 <sswastik60@gmail.com>
2025-06-13 00:36:36 +0530

    Odd and even

---
+++ oddandeven.java
@@ -0,0 +1,25 @@
+package Java_practice;
+import java.util.Scanner;
+
+public class oddandeven {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number to be checked for odd and even: ");
+        int num=in.nextInt();
+        boolean ans=iseven(num);
+        if(ans==true){
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+
+    }
+    static boolean iseven(int num){
+        if(num%2==0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
