b350be96c16694fb31a0be10750c9049aa13b6af
Swastik Sharma <130757809+Sswastik60@users.noreply.github.com>
2025-06-15 18:39:56 +0530

    Update with efficient code leetcode1672.java

--- leetcode1672.java
+++ leetcode1672.java
@@ -12,3 +12,26 @@ class Solution {
         return Collections.max(sum);      
     }    
 }//very very conceptual question i completed using ArrayList<Integer> sum= new ArrayList<>(); and Collections is a class to help find min and max values in an array list.
+
+
+
+/* class Solution {
+    public int maximumWealth(int[][] arr) {
+        int sum = 0;
+        int sum1 = 0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                sum = sum + arr[i][j];                              //This is a much efficient way very good solution
+            }
+            if(sum>=sum1){
+                sum1 = sum;
+                sum = 0;
+            }
+            else{
+                sum = 0;
+            }
+        }
+        return sum1;
+        
+    }
+}*/
