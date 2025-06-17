0af68d5005e97f7cd90f23a2b760022c5a96fee0
Swastik Sharma <130757809+Sswastik60@users.noreply.github.com>
2025-06-17 19:54:20 +0530

    efficient code lc1732.java

--- lc1732.java
+++ lc1732.java
@@ -14,3 +14,21 @@ class Solution {
         return Collections.max(altitude);
     }
 }
+
+//much efficient code
+/*class Solution {
+  public int largestAltitude(int[] gain) {
+    int currentAltitude = 0;
+    // Highest altitude currently is 0.
+    int highestPoint = currentAltitude;
+
+    for (int altitudeGain : gain) {
+      // Adding the gain in altitude to the current altitude.
+      currentAltitude += altitudeGain;
+      // Update the highest altitude.
+      highestPoint = Math.max(highestPoint, currentAltitude);
+    }
+
+    return highestPoint;
+  }
+}*/
